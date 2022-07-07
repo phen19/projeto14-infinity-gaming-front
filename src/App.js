912339135a8a0f8140de91088c3c6bbbc2cb4242
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-07-07 19:56:12 -0300

    Refactor: Changes in the organization files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./reset.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Login.js"
-import SignUp from "./SignUp.js"
-import Products from "./Products.js"
-import Cart from "./Cart.js"
-import CheckOut from "./CheckOut.js"
+import Login from "./Pages/Login/Login.js"
+import SignUp from "./Pages/SignUp/SignUp.js"
+import Products from "./Pages/Products/Products.js"
+import Cart from "./Pages/Cart/Cart.js"
+import CheckOut from "./Pages/CheckOut/CheckOut.js"
 
 export default function App() {
 
@@ -13,11 +12,11 @@ export default function App() {
         
             <BrowserRouter>
                 <Routes>
-                <Route path ="/" element={<Login />}/>
-                <Route path="/sign-up" element={<SignUp />}/>
-                <Route path="/products" element={<Products />}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/check-out" element={<CheckOut/>}/>
+                    <Route path ="/" element={<Login />}/>
+                    <Route path="/sign-up" element={<SignUp />}/>
+                    <Route path="/products" element={<Products />}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/check-out" element={<CheckOut/>}/>
                 </Routes>
             </BrowserRouter>
         
---
+++ src/Pages/SignUp/SignUp.js
@@ -0,0 +1,26 @@
+export default function SignUp() {
+    return (
+        <>
+            <h1>Aparece ai por favor</h1>
+            <form>
+                <input
+                    type="tetx"
+                    placeholder="Nome"
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                />
+                <input
+                    type="password"
+                    placeholder="Confirme sua senha"
+                />
+                <button>Cadastrar</button>
+            </form>
+        </>
+    );
+}
