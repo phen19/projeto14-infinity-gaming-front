66ae4c2d70aa1e8d612ade5cc3ebbbbe6b944056
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-07-07 22:09:32 -0300

    Chore: Function handles user registration

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import SignUp from "./Pages/SignUp/SignUp.js"
 import Products from "./Pages/Products/Products.js"
 import Cart from "./Pages/Cart/Cart.js"
 import CheckOut from "./Pages/CheckOut/CheckOut.js"
+import Home from "./Pages/Home/Home.js";
 
 export default function App() {
 
 
     return (
-        
-            <BrowserRouter>
-                <Routes>
-                    <Route path ="/" element={<Login />}/>
-                    <Route path="/sign-up" element={<SignUp />}/>
-                    <Route path="/products" element={<Products />}/>
-                    <Route path="/cart" element={<Cart/>}/>
-                    <Route path="/check-out" element={<CheckOut/>}/>
-                </Routes>
-            </BrowserRouter>
-        
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/check-out" element={<CheckOut />} />
+            </Routes>
+        </BrowserRouter>
+
     );
 
 
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,26 +1,103 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+
 export default function SignUp() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const navigate = useNavigate();
+
+    function handleSignUp(event) {
+        event.preventDefault();
+
+        const promise = axios.post("http://localhost:5000/sign-up", { name, email, password, confirmPassword });
+
+        promise.then(() => navigate("/"));
+        promise.catch((res) => alert(`${res.response.data}`));
+    }
+
     return (
-        <>
-            <h1>Aparece ai por favor</h1>
-            <form>
+        <Container>
+            <h1>Infinity Gaming </h1>
+            <form onSubmit={handleSignUp}>
                 <input
                     type="tetx"
                     placeholder="Nome"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
                 />
                 <input
                     type="email"
                     placeholder="Email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                 />
                 <input
                     type="password"
                     placeholder="Senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                 />
                 <input
                     type="password"
                     placeholder="Confirme sua senha"
+                    value={confirmPassword}
+                    onChange={e => setConfirmPassword(e.target.value)}
                 />
-                <button>Cadastrar</button>
+                <button type="submit">Cadastrar</button>
             </form>
-        </>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-image: linear-gradient( 150deg, #4776E6, #8E54E9);
+
+    h1 {
+        width: 300px;
+        font-size: 30px;
+        text-align: center;
+        word-wrap: break-word;
+        margin-bottom: 40px;
+    }
+
+    form {
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    input {
+        width: 100%;
+        height: 30px;
+        padding-left: 10px;
+        border: none;
+        border-radius: 7px;
+    }
+
+    input::placeholder{
+        color: #785FE9;
+    }
+
+    button {
+        width: 100%;
+        height: 30px;
+        border: none;
+        border-radius: 7px;
+        color: #785FE9;
+    }
+`
