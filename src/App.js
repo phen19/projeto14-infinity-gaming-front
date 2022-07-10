3e915ed753657fd161a6c3a587eb5320c7357f47
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2022-07-10 18:21:06 -0300

    Feat: Created teh ContextAPI

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ import Products from "./Pages/Products/Products.js"
 import Cart from "./Pages/Cart/Cart.js"
 import CheckOut from "./Pages/CheckOut/CheckOut.js"
 import Home from "./Pages/Home/Home.js";
+import UserContext from "./Contexts/AuthContext.js";
+import { useState } from "react";
 
 export default function App() {
-
+    const [user, setUser] = useState([]);
 
     return (
-
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/sign-up" element={<SignUp />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/products" element={<Products />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/check-out" element={<CheckOut />} />
-            </Routes>
-        </BrowserRouter>
+        <UserContext.Provider value={{user, setUser}}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/check-out" element={<CheckOut />} />
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
 
     );
-
-
 }
